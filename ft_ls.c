84e0162439343f72a38c419d8851adecedbada91
Remy DESTREBECQ <rdestreb@e3r8p16.42.fr>
2014-11-26 21:23:38 +0100

    get grp&user id

--- ft_ls.c
+++ ft_ls.c
@@ -6,7 +6,7 @@
 /*   By: rdestreb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/25 10:13:32 by rdestreb          #+#    #+#             */
-/*   Updated: 2014/11/26 19:44:55 by rdestreb         ###   ########.fr       */
+/*   Updated: 2014/11/26 21:19:45 by rdestreb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,11 +37,14 @@ int	main(int ac, char **av)
 		if (ft_strncmp(file->d_name , ".", 1) != 0)
 		{
 			ft_putendl(file->d_name);
+			ft_putendl((char *)&file->d_type);
 			stat(file->d_name, info);
-			ft_putstr(ctime(&info->st_atime));
-			ft_putstr(ctime(&info->st_ctime));
+			//ft_putstr(ctime(&info->st_atime));
+			//ft_putstr(ctime(&info->st_ctime));
 			ft_putstr(ctime(&info->st_mtime));
-			ft_putstr(getpwuid(info->st_uid));
+			ft_putendl(getpwuid(info->st_uid)->pw_name);
+			ft_putendl(getgrgid(info->st_gid)->gr_name);
+			//ft_putendl()
 		}
 	}
 	if (closedir(dir) !=0)
